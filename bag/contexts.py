c5a4989a05f6425b4f611397488b311d034535bf
sherylg343 <sheryl.s.goldberg@gmail.com>
2020-10-29 01:47:58 +0000

    manual changes to sizes in products database

--- bag/contexts.py
+++ bag/contexts.py
@@ -20,6 +20,7 @@ def bag_contents(request):
             'quantity': quantity,
             'product': product,
         })
+        
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
