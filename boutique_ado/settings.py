acafd1d0bfaa586a20c63f28d0321b0035668a78
sherylg343 <sheryl.s.goldberg@gmail.com>
2020-11-18 04:13:52 +0000

    fixed bucket name

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -177,7 +177,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'ckz8780-boutique-ado'
+    AWS_STORAGE_BUCKET_NAME = 'sherylg343-boutique-ado'
     AWS_S3_REGION_NAME = 'us-east-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
